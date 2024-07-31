372019e9d9c98a60eec6320b4e2e43fd835719af
not baumpyru(Daniel Lee) <dlee0@terpmail.umd.edu>
2024-07-31 17:54:32 -0400

    Update script.py
    Condense code, also shows I practiced git using another instance

--- script.py
+++ script.py
@@ -1,4 +1,2 @@
 # Simple python script to practice Git and Github for INST326
-print('Simple Python Script')
-print('That helps practice git commands and using Github')
-print('GOODBYE!')
+print('Simple Python Script\nHelps practice git commands and using Github\nGOODBYE!')
