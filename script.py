f624631451a6a40e14255bfc98f5ad19b4bcf3c2
not baumpyru(Daniel Lee) <dlee0@terpmail.umd.edu>
2024-07-31 18:19:09 -0400

    Describe python script is simple and done quickly to practice git and Github when run

--- script.py
+++ script.py
@@ -1,2 +1,2 @@
 # Simple python script to practice Git and Github for INST326
-print('Simple Python Script\nHelps practice git commands and using Github\nGOODBYE!')
+print('Simple Python Script\nQuick and simple python script to practice git commands and using Github\nGOODBYE!')
